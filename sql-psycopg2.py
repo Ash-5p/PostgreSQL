11227de516898fa2bf94c8ed121affb34a625a49
Ashley Preece <ashleyjph@gmail.com>
2024-11-26 18:39:19 +0000

    Install psycopg2

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,6 +1,5 @@
 import psycopg2
 
-
 # connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
@@ -20,7 +19,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
 # Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
-cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen"from the "Track" table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
